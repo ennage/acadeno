8f12d8cd8b58489e165a86189eff65c6d228a678
heeyzelnut <azelleann.zel@gmail.com>
2026-04-23 07:11:37 +0800

    Remove obsolete model classes and update project cache

--- Backend/Models/Term.cs
+++
@@ -1,9 +0,0 @@
-namespace Acadeno.Backend.Models.Education.Terms
-{
-    public class Term
-    {
-        public string TermID { get; set; } = string.Empty;
-        public string TermName { get; set; } = string.Empty;
-        public bool IsCurrent { get; set; }
-    }
-}
